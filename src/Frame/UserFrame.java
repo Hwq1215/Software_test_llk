33e3624cdd1ad5f1fcc6a5d5d174ec521536317d
hwq <1305934019@qq.com>
2024-10-15 09:37:38 +0800

    user frame add

---
+++ src/Frame/UserFrame.java
@@ -0,0 +1,26 @@
+package Frame;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+//用户登录和注册
+public class UserFrame extends JFrame{
+    
+    public UserFrame(){
+        
+    }
+
+    public class LoginPanel extends JPanel{
+        LoginPanel(){
+            
+        }
+    }
+
+    public class ResgisterPanel extends JPanel{
+        ResgisterPanel(){
+
+        }
+    }
+
+}
+
+
